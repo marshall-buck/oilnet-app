6b27d5dc0d654fef3052ae3206b283a8a5df018a
Marshall Buck <onefootin@icloud.com>
2021-08-23 18:11:17 -0500

    cornerstone mocules installed

--- src/components/DicomContainer.vue
+++ src/components/DicomContainer.vue
@@ -1,9 +1,26 @@
 <template>
-  <div id="dicom-image" class="canvas-wh self-center"></div>
+  <div ref="dicom" class="canvas-wh self-center"></div>
 </template>
 
 <script>
-export default {};
+import * as cornerstone from 'cornerstone-core';
+import { onMounted, ref } from '@vue/runtime-core';
+
+export default {
+  setup() {
+    const dicom = ref(null);
+
+    onMounted(() => {
+      cornerstone.enable(dicom.value);
+      console.log(cornerstone);
+      console.log(dicom.value);
+    });
+
+    return {
+      dicom,
+    };
+  },
+};
 </script>
 
 <style></style>
